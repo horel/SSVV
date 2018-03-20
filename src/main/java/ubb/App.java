4a487ab409ed5c13e1bca634a6ef93275d347b73
Horatiu <horatiu.istovan@gmail.com>
2018-03-20 12:54:36 +0200

    initial project

---
+++ src/main/java/ubb/App.java
@@ -0,0 +1,19 @@
+package ubb;
+
+
+import ubb.ui.LaboratoriesUI;
+
+import java.io.IOException;
+
+public class App {
+
+    public static void main(String[] args) {
+    	LaboratoriesUI view  = new LaboratoriesUI();
+
+        try {
+            view.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
